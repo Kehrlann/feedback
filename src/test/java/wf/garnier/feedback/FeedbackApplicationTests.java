90730d5b030bfa05f05004329e167e0635a624c9
Daniel Garnier-Moiroux <git@garnier.wf>
2024-08-07 11:56:33 +0200

    tests: trim down FeedbackApplicationTests

--- src/test/java/wf/garnier/feedback/FeedbackApplicationTests.java
+++ src/test/java/wf/garnier/feedback/FeedbackApplicationTests.java
@@ -12,19 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Import(EmulatorConfiguration.class)
 class FeedbackApplicationTests {
-
-	@Autowired
-	MockMvc mvc;
 
 	@Autowired
 	WebClient webClient;
@@ -41,11 +35,6 @@ class FeedbackApplicationTests {
 		sessionRepository.save(new Session("Test session", true));
 		sessionRepository.save(new Session("Other test session", true));
 		sessionRepository.save(new Session("Inactive session", false));
-	}
-
-	@Test
-	void index() throws Exception {
-		mvc.perform(get("/")).andExpect(status().is2xxSuccessful());
 	}
 
 	@Test
