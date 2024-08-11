16f1b085b5368ce6c06c9ab589ce33dba9597955
Daniel Garnier-Moiroux <git@garnier.wf>
2024-08-11 11:52:12 +0200

    feat: navigate from home to session view

--- src/test/java/wf/garnier/feedback/FeedbackApplicationTests.java
+++ src/test/java/wf/garnier/feedback/FeedbackApplicationTests.java
@@ -1,6 +1,7 @@
 package wf.garnier.feedback;
 
 import com.gargoylesoftware.htmlunit.html.DomNode;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.jupiter.api.Test;
 
@@ -12,9 +13,19 @@ class FeedbackApplicationTests extends TestBase {
 	void listsSessions() throws Exception {
 		HtmlPage htmlPage = webClient.getPage("/");
 
-		var sessions = htmlPage.querySelectorAll("li").stream().map(DomNode::getTextContent).toList();
+		var sessions = htmlPage.querySelectorAll("li").stream().map(DomNode::getTextContent).map(String::trim).toList();
 
 		assertThat(sessions).containsExactly("Other test session", "Test session");
+	}
+
+	@Test
+	void viewSession() throws Exception {
+		HtmlPage htmlPage = webClient.getPage("/");
+
+		htmlPage = ((HtmlAnchor) htmlPage.querySelector("li > a")).click();
+
+		var sessionTitle = htmlPage.querySelector("h1").getTextContent();
+		assertThat(sessionTitle).isEqualTo("Other test session");
 	}
 
 }
