62096018d512658fda4912c0109845f63be0e821
Daniel Garnier-Moiroux <git@garnier.wf>
2024-08-13 16:59:14 +0200

    refactor: move SessionControllerTests -> SessionWebTests

--- src/test/java/wf/garnier/feedback/SessionWebTests.java
+++ src/test/java/wf/garnier/feedback/SessionWebTests.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class SessionControllerTests extends TestBase {
+class SessionWebTests extends TestBase {
 
 	Session session;
 
