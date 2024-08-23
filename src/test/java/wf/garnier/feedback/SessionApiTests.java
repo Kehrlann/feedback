92f3fa08b9bb98e8b24fa087db4c390aa4fe15b7
Daniel Garnier-Moiroux <git@garnier.wf>
2024-08-23 13:25:00 +0200

    tests: polish SessionApiTests

--- src/test/java/wf/garnier/feedback/SessionApiTests.java
+++ src/test/java/wf/garnier/feedback/SessionApiTests.java
@@ -3,6 +3,7 @@ package wf.garnier.feedback;
 import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -40,34 +41,39 @@ class SessionApiTests extends TestBase {
 			.andExpect(cookie().maxAge("voter-id", 60 * 60 * 24 * 7));
 	}
 
-	@Test
-	void addVote() throws Exception {
-		mvc.perform(post("/session/" + savedSession.getSessionId() + "/vote").with(csrf())
-			.cookie(cookie)
-			.param("feedback", "Fun")).andExpect(status().is2xxSuccessful());
+	@Nested
+	class AddVote {
 
-		mvc.perform(get("/session/" + savedSession.getSessionId() + "/vote"))
-			.andExpect(status().is2xxSuccessful())
-			.andExpect(jsonPath("$.length()").value(1));
-	}
+		@Test
+		void success() throws Exception {
+			mvc.perform(post("/session/" + savedSession.getSessionId() + "/vote").with(csrf())
+				.cookie(cookie)
+				.param("feedback", "Fun")).andExpect(status().is2xxSuccessful());
 
-	@Test
-	void addVoteSessionDoesNotExist() throws Exception {
-		mvc.perform(post("/session/does-not-exist/vote").with(csrf()).cookie(cookie).param("feedback", "Fun"))
-			.andExpect(status().isNotFound());
-	}
+			mvc.perform(get("/session/" + savedSession.getSessionId() + "/vote"))
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(jsonPath("$.length()").value(1));
+		}
 
-	@Test
-	void addVoteRequiresCookie() throws Exception {
-		mvc.perform(post("/session/" + savedSession.getSessionId() + "/vote").with(csrf()).param("feedback", "Fun"))
-			.andExpect(status().isBadRequest());
-	}
+		@Test
+		void sessionDoesNotExist() throws Exception {
+			mvc.perform(post("/session/does-not-exist/vote").with(csrf()).cookie(cookie).param("feedback", "Fun"))
+				.andExpect(status().isNotFound());
+		}
 
-	@Test
-	void addVoteOnlyAllowedFeedbackChoices() throws Exception {
-		mvc.perform(post("/session/" + savedSession.getSessionId() + "/vote").with(csrf())
-			.cookie(cookie)
-			.param("feedback", "This is not allowed")).andExpect(status().isBadRequest());
+		@Test
+		void noCookie() throws Exception {
+			mvc.perform(post("/session/" + savedSession.getSessionId() + "/vote").with(csrf()).param("feedback", "Fun"))
+				.andExpect(status().isBadRequest());
+		}
+
+		@Test
+		void invalidFeedbackChoice() throws Exception {
+			mvc.perform(post("/session/" + savedSession.getSessionId() + "/vote").with(csrf())
+				.cookie(cookie)
+				.param("feedback", "This is not allowed")).andExpect(status().isBadRequest());
+		}
+
 	}
 
 	@Test
